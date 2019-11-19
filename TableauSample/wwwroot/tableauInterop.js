dc3ba938dd790bcaff1093832a4319d5a829d74c
Howard Richards <dev@conficient.com>
2019-11-19 09:05:28 +0000

    Add project files.

---
+++ TableauSample/wwwroot/tableauInterop.js
@@ -0,0 +1,19 @@
+﻿
+// tableau invoker for Blazor
+// I've been lazy and added this to the window object
+
+window.initViz = function (containerDiv, url) {
+    // containerDiv: element to update - use @Ref in Blazor to get this
+    console.log("initViz called for " + url);
+    if (!containerDiv) throw "no container provided";
+    if (!url) throw "no url provided";
+
+    var options = {
+        hideTabs: true,
+        onFirstInteractive: function () {
+            console.log("Run this code when the viz has finished loading.");
+        }
+    };
+    console.log("initViz calling .Viz()");
+    var viz = new tableau.Viz(containerDiv, url, options);
+}
